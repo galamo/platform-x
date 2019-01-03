4fa369555c3a4945a3a2cb1f6005e82b91f6eace
Asaf <foo@bar.com>
2019-01-03 10:06:42 +0200

    did some changes

--- backend/start-server.js
+++ backend/start-server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3000; // Feel free to change in order to deploy multiple servers at once
 
 app.get("/", express.static("html/index.html"));
 // app.get('/', (req, res) => res.send('Hello World!'))
